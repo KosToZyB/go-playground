a50eea30a2ecc7caf821f456e121f068f3f5cfdf
Veselkov Konstantin <kostozyb@gmail.com>
2018-01-25 09:55:27 +0400

    add graceful shutdown

---
+++ 2-graceful-shutdown/README.md
@@ -0,0 +1,1 @@
+graceful shutdown from interview with Alexandr Shchukin: https://www.youtube.com/watch?v=fb56Hr93-Ms
---
+++ 2-graceful-shutdown/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(done chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	select {
+	case <-done:
+        fmt.Println("done")
+		return
+	default:
+		fmt.Println("begin")
+		time.Sleep(2 * time.Second)
+		fmt.Println("end")
+	}
+}
+
+func main() {
+	done := make(chan struct{})
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go worker(done, wg)
+
+	time.Sleep(1 * time.Second)
+	close(done)
+
+	wg.Wait()
+	fmt.Println("ok")
+}
